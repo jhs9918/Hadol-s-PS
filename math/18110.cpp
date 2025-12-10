5097755b3e5a20a82e49d69745258a7a1703f223
jhs9918 <jhs9918@naver.com>
2025-12-10 22:24:20 +0900

    Solve: math/18110.cpp

--- math/18110.cpp
+++ math/18110.cpp
@@ -7,6 +7,7 @@
 #include <map>
 #include <set>
 #include <cmath>
+#include <numeric>
 using namespace std;
 
 /*
@@ -17,13 +18,38 @@ using namespace std;
  * Category: Math
  * Difficulty: Silver IV
  */
+double trimmedMean(vector<int> arr, double trimRatio) {
+    int n = arr.size();
+    sort(arr.begin(), arr.end());
+
+    int trimCount = (int)round(n * trimRatio);
+    int start = trimCount;
+    int end = n - trimCount;
+
+    double sum = accumulate(arr.begin() + start, arr.begin() + end, 0.0);
+    double average = sum / (end - start);
+
+    // 소수점 둘째 자리에서 반올림
+    return (int)round(average);
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+    int n; cin>>n;
 
-    // 코드 작성
+    // 의견이 없으면 난이도 0
+    if (n == 0) {
+        cout << 0 << '\n';
+        return 0;
+    }
 
+    int d;
+    vector<int> v;
+    while(n--) {
+        cin >>d; v.push_back(d);
+    }
+    cout << trimmedMean(v, 0.15) << '\n';
     return 0;
 }
